247d47d7858db25490cf7f64c2eccc88dde27481
Francesca Pavan <francescapavan7620@gmail.com>
2022-09-12 16:10:48 -0300

    funcionalidad final 12/08/22 terminada (sin testear)

--- final-12-08-22/src/code/Supermercado.java
+++ final-12-08-22/src/code/Supermercado.java
@@ -13,23 +13,54 @@ public class Supermercado {
         Supermercado.criterioRecargo = criterioRecargo;
     }
 
-    public Boolean chequarElemento(Elemento e){
-
+    public Boolean chequarElemento(Elemento e) {
+        for (Seccion s : secciones) {
+            if (s.acepta(e)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean chequearPedido(Pedido p){
         for (Elemento e : p.getElementos()){
-            boolean accepted = false;
-            for (Seccion s : secciones){
-                if (s.acepta(e)){
-                    accepted = true;
-                    break;
-                }
-            }
-            if (!accepted)
+            if (! chequarElemento(e))
                 return false;
         }
         return true;
+    }
+
+    public Pedido copiaAceptados(Pedido p){
+        Pedido pedidoNuevo = new Pedido(p.getNombreCliente(), p.getDireccion(),new ArrayList<>(),p.getNroLocal());
+        for (Elemento e : p.getElementos()){
+            if (chequarElemento(e)){
+                pedidoNuevo.addElemento(e);
+            }
+        }
+        return pedidoNuevo;
+    }
+
+    public Seccion aceptaTodo(Pedido p){
+        for (Seccion s: secciones){
+            boolean all = true;
+            for (Elemento e: p.getElementos()){
+                if (!s.acepta(e)){
+                    all = false;
+                    break;
+                }
+            }
+            if (all)
+                return s;
+        }
+        return null;
+    }
+
+    public Float totalCost(Pedido p){
+        Float cost = 0f;
+        for (Elemento e : p.getElementos()){
+            cost += criterioRecargo.aplicar(e);
+        }
+        return cost;
     }
 
 
